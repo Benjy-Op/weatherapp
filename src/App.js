ebf7e8977c274dfde477a398a8d9c7e6e64ceebb
Benjy-Op <mj993600@gmail.com>
2023-04-10 12:58:22 +0300

    done project need to correct error on api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const apiKey = "afa251c0c85eaf1588fe5ee2238ead24"
+  const  [weatherData, setWeatherData] = useState ([{}])
+  const [city, setCity] = useState('')
+
+  const getWeather = (event) => {
+    if (event.key === 'Enter') {
+      fetch (`api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`).then(
+        response => response.json()
+      ).then(
+        data => {
+          setWeatherData(data)
+          setCity ('')
+        }
+      )
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <input 
+      className='input' 
+      placeholder='Enter city...'
+      onChange={e => setCity(e.target.value)}
+      value = {city}
+      onKeyDown = {getWeather}
+      />
+
+      {typeof weatherData.main === 'undefined' ? (
+        <div>
+          <p>Welcome to weather app! Enter in a city to get weather info.</p>
+        </div>  
+      ) : (
+        <div className='weather-data'>
+          <p className='city'>{weatherData.name}</p>
+          <p className='temp'>{Math.round(weatherData.main.temp)} F</p>
+          <p className='weather'>{weatherData.weather[0].main}</p>
+        </div>  
+      )}
+
+      {weatherData.cod === '404' ? (
+        <p>city not found.</p>
+      ) : (
+        <></>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
