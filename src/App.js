988b92b0e4b8174de4db6cccd58bb8dd4af4bd72
Benjy-Op <mj993600@gmail.com>
2023-04-11 13:44:23 +0300

    fixed api problem

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const getWeather = (event) => {
     if (event.key === 'Enter') {
-      fetch (`api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`).then(
+      fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`).then(
         response => response.json()
       ).then(
         data => {
